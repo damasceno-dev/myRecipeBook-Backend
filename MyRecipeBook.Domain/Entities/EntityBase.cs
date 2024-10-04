9a326ef50878be78948eb9696ae2af655f4975d9
fernandodamasceno <damasceno.developer21@gmail.com>
2024-10-04 12:31:38 -0300

    feat: add entityBase class for all entities

---
+++ MyRecipeBook.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+namespace MyRecipeBook.Domain.Entities;
+
+public class EntityBase
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    public bool Active { get; set; } = true;
+}
--- MyRecipeBook.Domain/Entities/User.cs
+++ MyRecipeBook.Domain/Entities/User.cs
@@ -2,11 +2,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyRecipeBook.Domain.Entities;
 
-public class User
+public class User : EntityBase
 {
-    public Guid Id { get; set; }
-    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
-    public bool Active { get; set; } = true;
     [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
     [MaxLength(50)]
