3bd561b1da92e6290a713282963ed4f88750ccbe
fernandodamasceno <damasceno.developer21@gmail.com>
2024-10-14 15:47:47 -0300

    add lowercase urls

--- MyRecipeBook.API/Program.cs
+++ MyRecipeBook.API/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
