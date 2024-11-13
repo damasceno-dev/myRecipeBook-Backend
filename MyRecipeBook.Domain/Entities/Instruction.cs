8ec65b9cea94f6d22ce08c06d9a4d6b58021e5f2
fernandodamasceno <damasceno.developer21@gmail.com>
2024-11-13 09:29:30 -0300

    add max length on instruction entity in the database

--- MyRecipeBook.Domain/Entities/Instruction.cs
+++ MyRecipeBook.Domain/Entities/Instruction.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MyRecipeBook.Domain.Entities;
 
 public class Instruction : EntityBase
 {
     public int Step { get; set; }
+    [MaxLength(2000)]
     public string Text { get; set; } = string.Empty;
     public Guid RecipeId { get; set; }
 }
---
+++ MyRecipeBook.Infrastructure/Migrations/20241113122842_InstructionTextMaxLength.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyRecipeBook.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstructionTextMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Instructions",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Instructions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000);
+        }
+    }
+}
